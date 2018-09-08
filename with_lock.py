c856253d2796635a6c52d8fc37a056f5cf9f018f
Melnik Ivan <melnic.ivan@gmail.com>
2018-09-08 19:12:19 +0300

    Add threads with lock example

---
+++ with_lock.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Advanced Python Courses. Homework #3"""
+
+from threading import Thread, Lock
+from time import sleep
+
+from common import is_even, BaseOddEven
+
+
+class OddEven(BaseOddEven, Thread):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.lock = Lock()
+
+    def run(self):
+        while self.pool:
+            self.lock.acquire()
+            if self.pool:
+                if self.even == is_even(self.pool[-1]):
+                    print(self.message_template.format(self.pool.pop()))
+            self.lock.release()
+            sleep(0.0005)
